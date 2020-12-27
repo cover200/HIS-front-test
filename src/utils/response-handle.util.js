04793f430b8c3533ab82e5c154692c029aed3546
Loklek <potsawat_3232@hotmail.com>
2020-12-27 09:36:16 +0700

    move file to src/

--- src/utils/response-handle.util.js
+++ src/utils/response-handle.util.js
@@ -1,13 +1,13 @@
 export const responseHandle = async (dispatch, request) => {
   try {
-    const data = await request;
-    return data;
+    const res = await request;
+    return res;
   } catch (e) {
     errorHandle(dispatch, e);
   }
 };
 
-export const errorHandle = (dispatch, error) => {
+const errorHandle = (dispatch, error) => {
   const { statusText, status, data } = error;
   console.error(status);
   console.error(statusText);
